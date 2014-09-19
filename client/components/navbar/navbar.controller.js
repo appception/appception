8517d89d721617e475b193b3bbc228459092672f
wykhuh <wykhuh@users.noreply.github.com>
2014-09-19 11:10:07 -0700

    (feat/auth) Attempt to fix login with Mongo turned on

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -30,6 +30,24 @@ angular.module('appceptionApp')
       return route === $location.path();
     };
 
+    $scope.login = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+        Auth.login({
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          // Logged in, redirect to files
+          $location.path('/files');
+        })
+        .catch( function(err) {
+          $scope.errors.other = err.message;
+        });
+      }
+    };
+
      $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
