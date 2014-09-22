26f7b738084a7bebf0194a1a7996f021fbb9ef86
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-09-22 11:12:46 -0700

    angular testing and some styling

---
+++ e2e/newproject/newproject.po.js
@@ -0,0 +1,13 @@
+/**
+ * This file uses the Page Object pattern to define the main page for tests
+ * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
+ */
+
+'use strict';
+
+var NewProjectsPage = function() {
+  // this.signin = element(by.cssContainingText('button', 'Sign in with Github'));
+};
+
+module.exports = new NewProjectsPage();
+
