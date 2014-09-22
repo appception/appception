cd12a15c113a7dae9c1a28b62aa8137430f5fa74
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-09-22 16:27:40 -0700

    added loading signification to all projects

--- client/app/projects/allProjects/allProjects.controller.js
+++ client/app/projects/allProjects/allProjects.controller.js
@@ -4,19 +4,20 @@ angular.module('appceptionApp')
   .controller('AllProjectsCtrl', function ($q, $scope, github, Auth) {
 
     $scope.projects;
+    $scope.loading = false;
 
     // Check to see if currentUser exists. If it does exist, get all of its repos. If it doesn't exist, print an error on the screen
   	Auth.isLoggedInAsync(function(boolean) {
+      $scope.loading = true;
     	if(boolean === true){
     		var user = Auth.getCurrentUser()
-    		console.log('user', user.github.login)
     		github.getRepos(user.github.login).then(function(res){
 	      	$scope.projects = res.data;
-	      	console.log('res.data', res)
-	      	console.log('$scope.projects', $scope.projects)
+          $scope.loading = false;
 	    	})
     	}else {
-    		$scope.projects = 'Sorry, no projects have been found'
+    		$scope.projects = 'Sorry, no projects have been found';
+        $scope.loading = false;
     	}
 	  });
 
