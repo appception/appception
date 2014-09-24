f28a321d01519049b246bbeebdf37cf9732e2320
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-09-24 14:52:48 -0700

    more WIP styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,7 +244,7 @@ module.exports = function (grunt) {
       target: {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
-        exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/']
+        exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap/]
       }
     },
 
