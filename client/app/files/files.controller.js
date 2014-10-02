873246d9d417713e22c77473397200d1a658a2e7
wykhuh <wykhuh@users.noreply.github.com>
2014-10-02 15:42:55 -0700

    (feat/projects) ) add promises to files controller >  exportLocalDB()

--- client/app/files/files.controller.js
+++ client/app/files/files.controller.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('appceptionApp')
-  .controller('FilesCtrl', function ($scope, $stateParams, $timeout, github, Auth, $state) {
+  .controller('FilesCtrl', function ($scope, $stateParams, $timeout, github, Auth, $state, $q) {
 
-  	$scope.repoName = $stateParams.repoName;
+    $scope.repoName = $stateParams.repoName;
     $scope.isDeployed = false;
     $scope.checkBranches = false;
     $scope.success = false;
     $scope.failure = false;
     $scope.committing = false;
 
-		Auth.isLoggedInAsync(function(boolean) {
-    	if(boolean === true){
-	  		var user = Auth.getCurrentUser()
-	  		$scope.username = user.github.login
+    Auth.isLoggedInAsync(function(boolean) {
+      if(boolean === true){
+        var user = Auth.getCurrentUser()
+        $scope.username = user.github.login
 
         // Get all the branches to look and see if they have a gh-pages branch for deployment
         github.getBranches($scope.username, $scope.repoName)
@@ -25,10 +25,10 @@ angular.module('appceptionApp')
             }
             $scope.checkBranches = true;
           });
-	  	}else {
-  			console.log('Sorry, an error has occurred while loading the user');
-  		}
-	  });
+      }else {
+        console.log('Sorry, an error has occurred while loading the user');
+      }
+    });
 
     $scope.addDeployBranch = function() {
       // Create a gh-pages branch
@@ -39,6 +39,7 @@ angular.module('appceptionApp')
         })
     }
 
+
     var filer = new Filer.FileSystem({
       name: 'files',
       provider: new Filer.FileSystem.providers.Fallback('makedrive')
@@ -47,62 +48,84 @@ angular.module('appceptionApp')
     var shell = filer.Shell();
 
     var exportLocalDB = function(callback){
+      var promises = [];
+      var results = [];
 
-      // get list of all files and directories in user's browsers local DB
+      // turn shell.ls callback into a promise
+      var defer = $q.defer();
+
+      // shell.ls get list of all files and directories in user's browsers local DB
       shell.ls('/', {recursive: true}, function(err, entries){
-        console.log('entries', entries[0])
-        var results = [];
+        // console.log('entries', entries[0])
         if (err) throw err;
-
+        // counter keeps track of the number of promises
+        var counter = 0;
 
         var traverseDirectory = function(item, fullpath){
-          var itemObj = {};
-
-          //  add path of directory to results
-          results.push({path: fullpath})
-          // console.log('directory:', fullpath);
 
           // loop through every item in a directory
-          item.contents.forEach(function(result, i){
+          angular.forEach(item.contents, function(entry, i){
             var entry = item.contents[i];
-            var itemPath = fullpath + '/' + entry.path
+            var itemPath = fullpath + '/' + entry.path;
 
-            // if item is a file, add  file path and content to results
+            // if item is a file, read the file,  
+            // and add  file path and content to promises
             if(entry.type === 'FILE'){
-              filer.readFile(itemPath, function(err, data){
-                // console.log('file2:', itemPath, data);
-                results.push({path: itemPath, content: data.toString()})
-              })
 
-            // if item is directory, recursively traverse the directory
+              (function(i) {
+                // turns filer.readFile callback into a promise
+                promises[i] = $q.defer();
+                filer.readFile(itemPath, function(err, data){
+                  if (err) {
+                    return promises[i].reject(err);
+                  } 
+                  // console.log('file2:', itemPath, data);
+                  promises[i].resolve({path: itemPath, content: data.toString()});
+                }) 
+              })(counter++)
+
+            // if item is directory, add directory path to promises, and 
+            //  recursively traverse the directory
             } else if (entry.type === 'DIRECTORY') {
+             // console.log('directory:', itemPath);
+              promises[counter]  = $q.defer();
+              promises[counter].resolve({path: itemPath});
+              counter++;
               traverseDirectory(entry, itemPath );
             }
-          })
 
+            // console.log(promises)
+
+          })
         }
 
+        // get all the files and directories for the root directory
         if(entries[0] && entries[0].type==="DIRECTORY"){
           traverseDirectory(entries[0], '/' + entries[0].path)
         } else {
           alert('You need a folder folder at the root of your project.')
         }
 
-        setTimeout(function(){
-          callback(results);
-        }, 1000);
-
-
+        // since there are two nested level of promises,
+        // push every  promise in 2nd level of promise (promises[])
+        // into the promise of the 1st level (REALpromises[]) 
+        var REALpromises = [];
+        angular.forEach(promises, function(promise) {
+          REALpromises.push(promise.promise);
+        })
+        defer.resolve($q.all(REALpromises));
+  
       });
+
+      return defer.promise;
+
     };
 
 
     $scope.getProjectFiles = function(){
 
-      var files = exportLocalDB(function(results){
-
-        console.log(results)
-
+      exportLocalDB().then(function(result){
+        console.log('result', result)
       });
 
     }
