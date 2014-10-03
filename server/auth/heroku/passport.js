df85e3dfac99b9bb576337ddba7f8e0fa31d6354
wykhuh <wykhuh@users.noreply.github.com>
2014-10-03 15:19:11 -0700

    (feat/auth) Begin adding Heroku Oauth

---
+++ server/auth/heroku/passport.js
@@ -0,0 +1,41 @@
+exports.setup = function (User, config) {
+  var passport = require('passport');
+  var HerokuStrategy = require('passport-heroku').Strategy;
+
+  passport.use(new HerokuStrategy({
+    clientID: config.heroku.clientID,
+    clientSecret: config.heroku.clientSecret,
+    callbackURL: config.heroku.callbackURL
+  },
+  function(token, tokenSecret, profile, done) {
+    // console.log('token', token);
+    User.findOne({
+      'heroku.id_str': profile.id
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        console.log('token !user', token)
+        exports.token = token
+        user = new User({
+          name: profile.displayName,
+          username: profile.username,
+          role: 'user',
+          provider: 'heroku',
+          heroku: profile._json
+        });
+        user.save(function(err) {
+          if (err) return done(err);
+          return done(err, user);
+        });
+      } else {
+        console.log('token user', token)
+        return done(err, user);
+      }
+    });
+    }
+  ));
+};
+
+exports.token = '';
--- server/auth/index.js
+++ server/auth/index.js
@@ -8,10 +8,13 @@ var User = require('../api/user/user.model');
 // Passport Configuration
 require('./local/passport').setup(User, config);
 require('./github/passport').setup(User, config);
+// require('./heroku/passport').setup(User, config);
 
 var router = express.Router();
 
+// sets up auth/provider routes
 router.use('/local', require('./local'));
 router.use('/github', require('./github'));
+router.use('/heroku', require('./heroku'));
 
 module.exports = router;
