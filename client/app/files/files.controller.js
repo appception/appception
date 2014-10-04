4bd8543f51f6088ab9cb0867182ab89ce48fe9f9
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-10-04 08:51:17 -0700

    code clean up

--- client/app/files/files.controller.js
+++ client/app/files/files.controller.js
@@ -31,11 +31,6 @@ angular.module('appceptionApp')
       }
     });
 
-    $scope.hideLoader = function() {
-      alert('hello')
-      $scope.nimbleLoader = false;
-    }
-
     $scope.addDeployBranch = function() {
       // Create a gh-pages branch
       github.createBranch($scope.username, $scope.repoName, 'master', 'gh-pages')
@@ -57,84 +52,14 @@ angular.module('appceptionApp')
         })
     }; // end addBranch()
 
-    /**************************
-     * NOTE: Aaron is putting this codeblock
-     * back in but commenting out.
-     * This was missing during a merge, and
-     * it seems like something we *might* want
-     * to keep around for a day or 2.
-     **************************/
-
-     
-    // var filer = new Filer.FileSystem({
-    //   name: 'files',
-    //   provider: new Filer.FileSystem.providers.Fallback('makedrive')
-    // });
-
-    // var shell = filer.Shell();
-
-    // var exportLocalDB = function(callback){
-
-    //   // get list of all files and directories in user's browsers local DB
-    //   shell.ls('/', {recursive: true}, function(err, entries){
-    //     console.log('entries', entries[0])
-    //     var results = [];
-    //     if (err) throw err;
-
-
-    //     var traverseDirectory = function(item, fullpath){
-    //       var itemObj = {};
-
-    //       //  add path of directory to results
-    //       results.push({path: fullpath})
-    //       // console.log('directory:', fullpath);
-
-    //       // loop through every item in a directory
-    //       item.contents.forEach(function(result, i){
-    //         var entry = item.contents[i];
-    //         var itemPath = fullpath + '/' + entry.path
-
-    //         // if item is a file, add  file path and content to results
-    //         if(entry.type === 'FILE'){
-    //           filer.readFile(itemPath, function(err, data){
-    //             // console.log('file2:', itemPath, data);
-    //             results.push({path: itemPath, content: data.toString()})
-    //           })
-
-    //         // if item is directory, recursively traverse the directory
-    //         } else if (entry.type === 'DIRECTORY') {
-    //           traverseDirectory(entry, itemPath );
-    //         }
-    //       })
-
-    //     }
-
-    //     if(entries[0] && entries[0].type==="DIRECTORY"){
-    //       traverseDirectory(entries[0], '/' + entries[0].path)
-    //     } else {
-    //       alert('You need a folder folder at the root of your project.')
-    //     }
-
-    //     setTimeout(function(){
-    //       callback(results);
-    //     }, 1000);
-
-
-    //   });
-    // };
-
-
     $scope.createCommit = function(message) {
       var message = prompt('Enter a commit message:')
       $scope.committing = true;
       indexedDB.exportLocalDB().then(function(filesArray) {
 
-        console.log(filesArray)
-
         filesArray.shift()
         for(var i = filesArray.length-1; i >= 0; i--) {
           if(!filesArray[i]["content"]){
-            console.log(filesArray[i])
             filesArray.splice(i, 1);
           }
         }
@@ -144,8 +69,6 @@ angular.module('appceptionApp')
           filesArray[i]["type"] = 'blob';
           filesArray[i]["path"] = filesArray[i]["path"].replace('/' + $scope.repoName + '/', '')
         }
-        // filesArray.shift()
-        console.log(filesArray)
 
         Auth.isLoggedInAsync(function(boolean) {
           if(boolean === true){
@@ -153,7 +76,6 @@ angular.module('appceptionApp')
             console.log('user: ', user)
             github.createCommit(user.github.login, $scope.repoName, message, filesArray)
               .then(function(res){
-                console.log('success!', res.data);
                 $scope.committing = false;
                 $scope.success = true;
               })
