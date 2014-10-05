e2d430e54debf891fcdafcad209dedfa55f23388
wykhuh <wykhuh@users.noreply.github.com>
2014-10-04 19:50:52 -0700

    (feat/auth) Starting heroku

--- client/app/heroku/heroku.controller.js
+++ client/app/heroku/heroku.controller.js
@@ -1,12 +1,23 @@
 'use strict';
 
 angular.module('appceptionApp')
-  .controller('HerokuCtrl', function ($scope, Auth, $window, $location) {
+  .controller('HerokuCtrl', function ($scope, Auth, $window, $location, github) {
     $scope.message = 'Hello';
 
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
+
+    $scope.deployHeroku = function(){
+      console.log('deploy Heroku')
+
+      github.getRepoFilesClient('wykhuh', 'test', 'master').then(function(files){
+        console.log(files)
+      });
+
+    }
+
+
 
 
   });
