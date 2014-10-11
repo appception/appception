f5b86345e3bb27f9c8087bfedb9f45f8b91ae69d
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-10-10 17:23:58 -0700

    dynamically adding generators

--- client/app/projects/newProject/newProject.controller.js
+++ client/app/projects/newProject/newProject.controller.js
@@ -6,7 +6,9 @@ angular.module('appceptionApp')
     $scope.creating = false;
 
     $scope.generator = 'beginner';
-    $scope.templates = '';
+    $scope.allTemplates;
+    $scope.renderedTemplate;
+    $scope.templateNames = [];
 
     $scope.deploymentProvider='';
 
@@ -38,10 +40,22 @@ angular.module('appceptionApp')
       })
     };
 
-    $scope.getTemplates = function() {
-      repoTemplates.getTemplates()
-      .then(function(res) {
-        $scope.templates = res.data
-      });
+    $scope.renderTemplate = function(repo) {
+      $scope.allTemplates.forEach(function(value) {
+        if(repo === value.name) {
+          console.log(value)
+          $scope.renderedTemplate = [value]
+        }
+      })
     }
+
+    repoTemplates.getTemplates()
+      .then(function(res) {
+        console.log(res.data)
+        $scope.allTemplates = res.data;
+        $scope.allTemplates.forEach(function(value) {
+          $scope.templateNames.push(value.name)
+        })
+        $scope.renderTemplate('beginner')
+      });
   });
